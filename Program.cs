6728d217d1878afd0287a4dfd1fb1b67ee572eab
Aravind <madrasmetallurgist@gmail.com>
2016-12-16 19:21:26 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System;
+using Microsoft.Win32;
+using System.IO;
+using System.Reflection;
+
+namespace FindDotNetVersion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetVersionFromRegistry();
+        }
+
+        public class LogWriter
+        {
+            private string m_exePath = string.Empty;
+            public LogWriter(string logMessage)
+            {
+                LogWrite(logMessage);
+            }
+            public void LogWrite(string logMessage)
+            {
+                m_exePath = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+                try
+                {
+                    using (StreamWriter w = File.AppendText(m_exePath + "\\" + "log.txt"))
+                    {
+                        Log(logMessage, w);
+                    }
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+
+            public void Log(string logMessage, TextWriter txtWriter)
+            {
+                try
+                {
+                    txtWriter.Write("\r\nLog Entry : ");
+                    txtWriter.WriteLine("{0} {1}", DateTime.Now.ToLongTimeString(),
+                        DateTime.Now.ToLongDateString());
+                    txtWriter.WriteLine("  :");
+                    txtWriter.WriteLine("  :{0}", logMessage);
+                    txtWriter.WriteLine("-------------------------------");
+                }
+                catch (Exception ex)
+                {
+                }
+            }
+        }
+        private static void GetVersionFromRegistry()
+        {
+            // Opens the registry key for the .NET Framework entry.
+            LogWriter Logi = new LogWriter("Stared....");
+            
+            using (RegistryKey ndpKey =
+                RegistryKey.OpenRemoteBaseKey(RegistryHive.LocalMachine, "").
+                OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+            {
+                // As an alternative, if you know the computers you will query are running .NET Framework 4.5 
+                // or later, you can use:
+                // using (RegistryKey ndpKey = RegistryKey.OpenBaseKey(RegistryHive.LocalMachine, 
+                // RegistryView.Registry32).OpenSubKey(@"SOFTWARE\Microsoft\NET Framework Setup\NDP\"))
+                foreach (string versionKeyName in ndpKey.GetSubKeyNames())
+                {
+                    if (versionKeyName.StartsWith("v"))
+                    {
+
+                        RegistryKey versionKey = ndpKey.OpenSubKey(versionKeyName);
+                        string name = (string)versionKey.GetValue("Version", "");
+                        string sp = versionKey.GetValue("SP", "").ToString();
+                        string install = versionKey.GetValue("Install", "").ToString();
+                        if (install == "") //no install info, must be later.
+                        {
+                            Console.WriteLine(versionKeyName + "  " + name);
+                            Logi.LogWrite(versionKeyName + "  " + name);
+                        }
+                        else
+                        {
+                            if (sp != "" && install == "1")
+                            {
+                                Console.WriteLine(versionKeyName + "  " + name + "  SP" + sp);
+                                Logi.LogWrite(versionKeyName + "  " + name + "  SP" + sp);
+                            }
+
+                        }
+                        if (name != "")
+                        {
+                            continue;
+                        }
+                        foreach (string subKeyName in versionKey.GetSubKeyNames())
+                        {
+                            RegistryKey subKey = versionKey.OpenSubKey(subKeyName);
+                            name = (string)subKey.GetValue("Version", "");
+                            if (name != "")
+                                sp = subKey.GetValue("SP", "").ToString();
+                            install = subKey.GetValue("Install", "").ToString();
+                            if (install == "") //no install info, must be later.
+                            {
+                                Console.WriteLine(versionKeyName + "  " + name);
+                                Logi.LogWrite(versionKeyName + "  " + name);
+                            }
+                            else
+                            {
+                                if (sp != "" && install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name + "  SP" + sp);
+                                    Logi.LogWrite("  " + subKeyName + "  " + name + "  SP" + sp);
+                                }
+                                else if (install == "1")
+                                {
+                                    Console.WriteLine("  " + subKeyName + "  " + name);
+                                    Logi.LogWrite("  " + subKeyName + "  " + name);
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
